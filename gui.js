a595aba76b85bb5b0ec3da94df79b60b45117ad0
Rui <rui.albuquerque.dorey@gmail.com>
2020-09-17 17:26:37 +0100

    fix speedY missing

--- gui.js
+++ gui.js
@@ -4,5 +4,5 @@ gui.add(uniforms, "colorIntensity");
 gui.add(uniforms, "shadowIntensity");
 gui.add(uniforms, "saturation");
 gui.add(uniforms, "speedX");
-gui.add(uniforms, "speedX");
+gui.add(uniforms, "speedY");
 gui.add(uniforms, "scale");
